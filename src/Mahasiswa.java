df3972793d42f3dd8f118d8ee09c3ad619d84e23
alvinlukmanNH <galinx20gmail.com>
2018-10-08 22:49:46 +0700

    Perkenalanmhs

---
+++ src/Mahasiswa.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama             : Alvin Lukman Nulhakim
+ * Nim              : 10117095
+ * Kelas            : IF-3
+ * Deskripsi        : Program Perkenalan diri mahasiswa/i
+ */
+public class Mahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.setNim("10117095");
+        mhs1.setNama("ALvin Lukman Nulhakim");
+        mhs1.perkenalkanDiri();
+
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.setNim("10110270");
+        mhs2.setNama("Udin Kasus");
+        mhs2.perkenalkanDiri();
+
+        Mahasiswa mhs3 = new Mahasiswa();
+        mhs3.setNim("10110271");
+        mhs3.setNama("Genejeh");
+        mhs3.perkenalkanDiri();
+
+        Mahasiswa mhs4 = new Mahasiswa();
+        mhs4.setNim("10110272");
+        mhs4.setNama("Muhammad Nur Awaluddin");
+        mhs4.perkenalkanDiri();
+
+    }
+
+    private void setNim(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setNama(String rizki_Adam_Kurniawan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void perkenalkanDiri() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
